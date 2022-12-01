34fb6ccf3e020459d2219ed40f177ed1a61cf1c2
Didrik Frimann Barroso Koren <dfkoren@studentmediene.no>
2022-12-01 23:49:03 +0100

    Add day 1 of aoc

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,41 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        var max = 0L
+        var current = 0L
+        for (item in input) {
+            current = (item.toLongOrNull()?.plus(current) ?:  if (current > max) {
+                max = current
+                0L
+            }
+            else 0L
+            )
+        }
+        return max
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val loads = mutableListOf<Long>()
+        var current = 0L
+        for (item in input) {
+            val readItem = item.toLongOrNull()
+            if (readItem == null) {
+                loads.add(current)
+                current = 0L
+            }
+            else {
+                current += readItem
+            }
+        }
+        if (current != 0L) {
+            loads.add(current)
+        }
+        return loads.sorted().takeLast(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000L)
+    check(part2(testInput) == 45000L)
 
     val input = readInput("Day01")
     println(part1(input))
