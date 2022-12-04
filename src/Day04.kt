2a68e7c09e54ee0b6d860b3b438e8ff0077b7263
Didrik Frimann Barroso Koren <dfkoren@studentmediene.no>
2022-12-04 13:42:32 +0100

    Add day4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,17 +1,41 @@
 fun main() {
     fun part1(input: List<String>): Long {
-        return input.size.toLong()
+        return input
+            .map { line ->
+                line.split(',')
+                    .map { range ->
+                        range.split('-')
+                            .map { endpoint -> endpoint.toLong() }
+                    }
+                    .map { longs ->
+                        LongRange(longs.first(), longs.last())
+                    }
+            }.count { longRanges ->
+                val intersect = longRanges.first().intersect(longRanges.last())
+                longRanges.first().toSet() == intersect || longRanges.last().toSet() == intersect
+            }.toLong()
     }
 
     fun part2(input: List<String>): Long {
-        return input.size.toLong()
-
+        return input
+            .map { line ->
+                line.split(',')
+                    .map { range ->
+                        range.split('-')
+                            .map { endpoint -> endpoint.toLong() }
+                    }
+                    .map { longs ->
+                        LongRange(longs.first(), longs.last())
+                    }
+            }.filter { longRanges ->
+                longRanges.first().intersect(longRanges.last()).isNotEmpty()
+            }.count().toLong()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 0L)
-    check(part2(testInput) == 0L)
+    check(part1(testInput) == 2L)
+    check(part2(testInput) == 4L)
 
     val input = readInput("Day04")
     println(part1(input))
