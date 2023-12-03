df578bf7e72229f9c5eca2b62603ddf3b2fde380
Didrik Frimann Barroso Koren <dfkoren@studentmediene.no>
2023-12-03 10:59:59 +0100

    Prepare some days

---
+++ aoc-2023/src/main/kotlin/nerok/aoc/aoc2023/day07/Day07.kt
@@ -0,0 +1,29 @@
+package nerok.aoc.aoc2023.day02
+
+import nerok.aoc.utils.GenericGrid
+import nerok.aoc.utils.Input
+import nerok.aoc.utils.Point
+import nerok.aoc.utils.append
+import kotlin.time.DurationUnit
+import kotlin.time.measureTime
+
+fun main() {
+    fun part1(input: List<String>): Long {
+        return input.size.toLong()
+    }
+
+    fun part2(input: List<String>): Long {
+        return input.size.toLong()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = Input.readInput("Day07_test")
+    check(part1(testInput) == 1L)
+    check(part2(testInput) == 2L)
+
+    val input = Input.readInput("Day07")
+    println("Part1:")
+    println(measureTime { println(part1(input)) }.toString(DurationUnit.SECONDS, 3))
+    println("Part2:")
+    println(measureTime { println(part2(input)) }.toString(DurationUnit.SECONDS, 3))
+}
