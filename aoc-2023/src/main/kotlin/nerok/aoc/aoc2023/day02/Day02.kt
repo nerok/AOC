3d83d3a4c45a912485a4b3dc659073739d115074
Didrik Frimann Barroso Koren <dfkoren@studentmediene.no>
2023-12-03 10:50:13 +0100

    Add day2

--- aoc-2023/src/main/kotlin/nerok/aoc/aoc2023/day02/Day02.kt
+++ aoc-2023/src/main/kotlin/nerok/aoc/aoc2023/day02/Day02.kt
@@ -1,51 +1,92 @@
-package nerok.aoc.aoc2023.day01
+package nerok.aoc.aoc2023.day02
 
 import nerok.aoc.utils.Input
 import kotlin.time.DurationUnit
 import kotlin.time.measureTime
 
-fun main() {
-    fun part1(input: List<String>): Long {
-        return input.map { line ->
-            line.filter { it.isDigit() }
-        }.sumOf { "${it.first()}${it.last()}".toLong() }
+data class RGB(
+    val red: Long,
+    val green: Long,
+    val blue: Long
+) {
+    fun mergeMax(other: RGB): RGB {
+        return RGB(
+            red = maxOf(this.red, other.red),
+            green = maxOf(this.green, other.green),
+            blue = maxOf(this.blue, other.blue),
+        )
     }
 
-    fun part2(input: List<String>): Any {
-        return input.sumOf { line ->
-            "${line.findFirstNumber()}${line.findLastNumber()}".toLong()
+    fun aboveLimit(limit: RGB): Boolean {
+        if (this.red > limit.red) return true
+        if (this.green > limit.green) return true
+        if (this.blue > limit.blue) return true
+        return false
+    }
+
+    fun power(): Long {
+        return this.red * this.green * this.blue
+    }
+}
+
+fun String.toRGB(): RGB {
+    var red = 0L
+    var green = 0L
+    var blue = 0L
+    this
+        .split(",")
+        .map { it.trim() }
+        .map {
+        if (it.contains("red")) {
+            red = it.split(" ").first().toLongOrNull() ?: 0L
         }
+        if (it.contains("green")) {
+            green = it.split(" ").first().toLongOrNull() ?: 0L
+        }
+        if (it.contains("blue")) {
+            blue = it.split(" ").first().toLongOrNull() ?: 0L
+        }
+    }
+    return RGB(red, green, blue)
+}
+
+fun main() {
+    fun part1(input: List<String>): Long {
+        val limit = RGB(12,13,14)
+        return input.map { game ->
+            val parsed = game.split(":")
+            val gameid = parsed.first().removePrefix("Game ").toLong()
+            val data = parsed.last().split(";").map { c ->
+                c.toRGB()
+            }.reduce { acc, rgb -> acc.mergeMax(rgb) }
+            gameid to data
+        }.filterNot {
+            it.second.aboveLimit(limit)
+        }.sumOf { it.first }
+    }
+
+    fun part2(input: List<String>): Long {
+        return input.map { game ->
+            val data = game
+                .split(":")
+                .last()
+                .split(";")
+                .map { c ->
+                    c.toRGB()
+                }
+                .reduce { acc, rgb -> acc.mergeMax(rgb) }
+            data.power()
+        }.sumOf { it }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput1 = Input.readInput("Day01_test1")
-    check(part1(testInput1) == 142L)
-    val testInput2 = Input.readInput("Day01_test2")
-    check(part2(testInput2) == 281L)
+    val testInput = Input.readInput("Day02_test")
+    check(part1(testInput) == 8L)
+    check(part2(testInput) == 2286L)
 
-    val input = Input.readInput("Day01")
+    val input = Input.readInput("Day02")
+    println("Part1:")
     println(measureTime { println(part1(input)) }.toString(DurationUnit.SECONDS, 3))
+    println("Part2:")
     println(measureTime { println(part2(input)) }.toString(DurationUnit.SECONDS, 3))
-}
-
-private fun String.findFirstNumber(): String {
-    return findAnyOf(
-        listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-    )!!.second.convertToDigit()
-}
-private fun String.findLastNumber(): String {
-    return findLastAnyOf(
-        listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-    )!!.second.convertToDigit()
-}
-private fun String.convertToDigit(): String {
-    return replace("one", "1")
-        .replace("two", "2")
-        .replace("three", "3")
-        .replace("four", "4")
-        .replace("five", "5")
-        .replace("six", "6")
-        .replace("seven", "7")
-        .replace("eight", "8")
-        .replace("nine", "9")
 }
