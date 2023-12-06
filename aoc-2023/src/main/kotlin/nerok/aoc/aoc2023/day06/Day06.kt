0ed02395d7b3e62797dd9c4e200863a074d3c420
Didrik Frimann Barroso Koren <dfkoren@studentmediene.no>
2023-12-06 18:34:46 +0100

    Add Day06

--- aoc-2023/src/main/kotlin/nerok/aoc/aoc2023/day06/Day06.kt
+++ aoc-2023/src/main/kotlin/nerok/aoc/aoc2023/day06/Day06.kt
@@ -5,18 +5,31 @@ import kotlin.time.DurationUnit
 import kotlin.time.measureTime
 
 fun main() {
+    fun distances(time: Long): List<Long> {
+        val returnList = emptyList<Long>().toMutableList()
+        for (i in 0..time) {
+            returnList.add((time-i) * i)
+        }
+        return returnList
+    }
+
     fun part1(input: List<String>): Long {
-        return input.size.toLong()
+        val times = input.first().removePrefix("Time:").split(" ").filter { it.isNotBlank() }.map { it.toLong() }
+        val distance = input.last().removePrefix("Distance:").split(" ").filter { it.isNotBlank() }.map { it.toLong() }
+        return times.zip(distance).map { game -> distances(game.first).filter { it > game.second }.size}.reduce { acc, i -> acc * i }.toLong()
     }
 
     fun part2(input: List<String>): Long {
-        return input.size.toLong()
+        val times = input.first().removePrefix("Time:").filterNot { it.isWhitespace() }.map { it.digitToInt().toLong() }.reduce { acc: Long, l: Long -> (acc * 10) + l }
+        val distance = input.last().removePrefix("Distance:").filterNot { it.isWhitespace() }.map { it.digitToInt().toLong() }.reduce { acc: Long, l: Long -> (acc * 10) + l }
+        val distanceList = distances(times)
+        return distanceList.filter { it > distance }.size.toLong()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = Input.readInput("Day06_test")
-    check(part1(testInput) == 1L)
-    check(part2(testInput) == 2L)
+    check(part1(testInput) == 288L)
+    check(part2(testInput) == 71503L)
 
     val input = Input.readInput("Day06")
     println("Part1:")
